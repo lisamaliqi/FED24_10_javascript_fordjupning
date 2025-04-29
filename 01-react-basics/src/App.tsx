b5279677f3fccccf648738d2e2800d85e85801d8
lisamaliqi <lisamaliqi@hotmail.com>
2025-04-29 18:02:50 +0200

    addiing new button that changes mom to dad...

--- 01-react-basics/src/App.tsx
+++ 01-react-basics/src/App.tsx
@@ -4,19 +4,22 @@ import { useState } from "react";
 function App() {
 	// let counter = 0;  // stateless
 	const [counter, setCounter] = useState(0); //counter is 0 when starting the page
+	const [msg, setMsg] = useState("Hi mom!");
 
 	//function that adds +1 to counter when user press "click me" button
 	const handleBtnClick = () => {
 		console.log("Counter before update:", counter);
 		setCounter(counter + 1);
+		console.log("Counter after update:", counter);
 	};
 
-	console.log("Counter after update:", counter);
 	console.log("App is being rendered, counter is:", counter);
 
 	return (
 		<div className="container">
 			<h1>01-react-basics</h1>
+
+			<p>{msg}</p>
 
 			<p>Counter: {counter}</p>
 
@@ -25,6 +28,11 @@ function App() {
 				in this case, when user clicks button, do handleBtnClick function
 			 */}
 			<button onClick={handleBtnClick} className="btn btn-primary">Click me</button>
+
+			{/*
+				when clicking the button, the "hi mom" text changes to "hi dad"
+			*/}
+			<button onClick={ () => setMsg("Hi dad!") } className="btn btn-warning">Hi dad?</button>
 		</div>
 	);
 };
