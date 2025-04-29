13de34d29e1fc36e85510f256cfc11a5ef0dc458
lisamaliqi <lisamaliqi@hotmail.com>
2025-04-29 17:53:15 +0200

    A very basic first React app with a stateful variable using the useState hook and a onClick-event.

--- 01-react-basics/src/App.tsx
+++ 01-react-basics/src/App.tsx
@@ -1,11 +1,32 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+	// let counter = 0;  // stateless
+	const [counter, setCounter] = useState(0); //counter is 0 when starting the page
+
+	//function that adds +1 to counter when user press "click me" button
+	const handleBtnClick = () => {
+		console.log("Counter before update:", counter);
+		setCounter(counter + 1);
+	};
+
+	console.log("Counter after update:", counter);
+	console.log("App is being rendered, counter is:", counter);
+
 	return (
-		<>
+		<div className="container">
 			<h1>01-react-basics</h1>
-		</>
+
+			<p>Counter: {counter}</p>
+
+			{/*
+				onClick = when user clicks the event
+				in this case, when user clicks button, do handleBtnClick function
+			 */}
+			<button onClick={handleBtnClick} className="btn btn-primary">Click me</button>
+		</div>
 	);
-}
+};
 
 export default App;
