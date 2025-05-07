6be1deae583a7797962171bc29640413f0252ee9
lisamaliqi <lisamaliqi@hotmail.com>
2025-05-07 19:00:00 +0200

    making clock work

--- 03-clock/src/App.tsx
+++ 03-clock/src/App.tsx
@@ -1,10 +1,26 @@
+import { useEffect, useState } from "react";
 import "./assets/scss/App.scss";
 
 function App() {
+
+	const [time, setTime] = useState("00:00:00");
+
+	useEffect(() => {
+		console.log("🔫 Starting clock...");
+
+		//interval goes off once and continue its interval but starting clock log goes off just one time
+		setInterval(() => {
+			setTime(new Date().toLocaleTimeString());
+			console.log("🕰️ Tick...");
+		}, 1000);
+	}, []); //this is not dependent on another function so it will only start once
+
+
+
 	return (
 		<div className="container">
 			<div className="display-1 font-monospace text-center">
-				00:13:37
+				{time}
 			</div>
 		</div>
 	);
