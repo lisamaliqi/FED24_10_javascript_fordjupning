6433764b59dd8b3ccabcdfbc79527c574c37d4b3
lisamaliqi <lisamaliqi@hotmail.com>
2025-05-07 16:37:48 +0200

    adding small changes to make it more personal for me

--- 02-react-simple-todos/src/App.tsx
+++ 02-react-simple-todos/src/App.tsx
@@ -1,12 +1,13 @@
+import { useState } from "react";
 import './App.css'
 
 function App() {
 
   return (
-    <div className="app">
-
+    <div className="container">
+		<h1>React Simple Todo</h1>
 	</div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
