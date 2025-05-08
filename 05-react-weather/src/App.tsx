c6ecdd8b9fbabbf31a7fd49e4039ad96bdbd6c60
lisamaliqi <lisamaliqi@hotmail.com>
2025-05-08 14:11:59 +0200

    weather app initial functionality (basically so that i can search for a city and get the data)

--- 05-react-weather/src/App.tsx
+++ 05-react-weather/src/App.tsx
@@ -1,13 +1,29 @@
 import Forecast from "./components/Forecast";
 import SearchCity from "./components/SearchCity";
 import "./assets/scss/App.scss";
+import { useState } from "react";
+import { WeatherReport } from "./services/OWMAPI.types";
+import { getCurrentWeather } from "./services/OWMAPI";
 
 function App() {
+
+	const [ currentWeather, setCurrentWeather ] = useState<WeatherReport | null>(null);
+
+	const handleSearch = async (location: string) => {
+		console.log('Someone wants the weather for...: ', location);
+
+		//call API and ask for weather in 'location'
+		const data = await getCurrentWeather(location);
+
+		//update current weather state with the weather in 'location'
+		setCurrentWeather(data);
+	};
+
 	return (
 		<div id="app" className="container">
-			<SearchCity />
+			<SearchCity onSearch={handleSearch}/>
 
-			<Forecast />
+			{currentWeather && <Forecast data={currentWeather}/>}
 		</div>
 	);
 };
--- 05-react-weather/src/components/SearchCity.tsx
+++ 05-react-weather/src/components/SearchCity.tsx
@@ -1,7 +1,27 @@
-const SearchCity = () => {
+import { useState } from "react";
+
+interface SearchCityProps {
+	onSearch: (location: string) => void
+};
+
+const SearchCity: React.FC<SearchCityProps> = ({ onSearch }) => {
+
+	const [ city, setCity ] = useState('');
+
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault();
+
+		//Pass 'city' to parent component (app)
+		//trim to remove whitespace before and after input
+		onSearch(city.trim());
+
+		// clear 'city' state
+		setCity('');
+	};
+
 	return (
 		<div id="search-wrapper">
-			<form id="search-form">
+			<form onSubmit={handleSubmit} id="search-form">
 				<div className="input-group">
 					<input
 						type="text"
@@ -9,9 +29,16 @@ const SearchCity = () => {
 						placeholder="Enter city to search for"
 						aria-label="City"
 						aria-details="Search for city to show current weather for."
+						onChange={e => setCity(e.target.value)}
+						value={city}
+						required
 					/>
 
-					<button type="submit" className="btn btn-success">
+					<button
+						type="submit"
+						className="btn btn-success"
+						disabled={city.trim().length < 3} //if input has less than 3 chars, button is disabled
+					>
 						🔍
 					</button>
 				</div>
