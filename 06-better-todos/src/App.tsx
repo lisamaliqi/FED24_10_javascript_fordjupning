c05f5ec0c553021220a4f7d55147ab707f56eb38
lisamaliqi <lisamaliqi@hotmail.com>
2025-05-09 15:29:24 +0200

    Added Routes and created HomePage, TodosPage and Navigation components.

--- 06-better-todos/src/App.tsx
+++ 06-better-todos/src/App.tsx
@@ -1,121 +1,24 @@
-import { useEffect, useState } from "react";
-import AddTodoForm from "./components/AddTodoForm";
-import TodoCounter from "./components/TodoCounter";
-import TodoList from "./components/TodoList";
-import * as TodosAPI from "./services/TodosAPI";
-import { NewTodo, Todo } from "./types/Todo";
 import "./assets/scss/App.scss";
 import  Container  from "react-bootstrap/Container";
-import  Alert  from "react-bootstrap/Alert";
-import  Spinner  from "react-bootstrap/Spinner";
+import { Routes, Route } from 'react-router';
+import TodosPage from "./pages/TodosPage";
+import HomePage from "./pages/HomePage";
+import Navigation from "./components/Navigation";
 
 
 function App() {
-	const [error, setError] = useState<string | false>(false);
-	const [isLoading, setIsLoading] = useState(true);
-	const [todos, setTodos] = useState<Todo[]>([]);
-
-	// One of two use-cases for useRef - remember a value between renders
-	// *WITHOUT* triggering a re-render when the value is updated
-	/*
-	const renderCountRef = useRef(0);
-	renderCountRef.current++;
-	console.log("I have rendered this many times:", renderCountRef.current);
-	*/
-
-	const getTodos = async () => {
-		// reset state
-		setError(false);
-		setIsLoading(true);
-		setTodos([]);
-
-		// make request to api
-		try {
-			const data = await TodosAPI.getTodos();
-			setTodos(data);
-		} catch (err) {
-			console.error("Error thrown when fetching todos:", err);
-			setError(err instanceof Error ? err.message : "It's not me, it's you");
-		}
-		setIsLoading(false);
-	}
-
-	const handleAddTodo = async (title: string) => {
-		const todo: NewTodo = {
-			title,
-			completed: false,
-		}
-		await TodosAPI.postTodo(todo);
-		getTodos();
-	}
-
-	const handleDeleteTodo = async (todo: Todo) => {
-		await TodosAPI.deleteTodo(todo.id);
-		getTodos();
-	}
-
-	const handleToggleTodo = async (todo: Todo) => {
-		await TodosAPI.updateTodo(todo.id, {
-			completed: !todo.completed,
-		});
-		getTodos();
-	}
-
-	const completedTodos = todos.filter(todo => todo.completed);
-	const incompleteTodos = todos.filter(todo => !todo.completed);
-
-	useEffect(() => {
-		getTodos();
-	}, []);
-
 	return (
-		<Container className="container py-2">
-			<h1>Better Todos</h1>
+		<div id="app">
+			<Navigation />
 
-			<AddTodoForm onAddTodo={handleAddTodo} />
-
-			{error && (
-				<Alert variant="danger">
-					{error}
-				</Alert>
-			)}
-
-			{isLoading && (
-				<Spinner animation="border" role="status">
-					<span className="visually-hidden">Loading...</span>
-				</Spinner>
-			)}
-
-			{!error && !isLoading && todos.length > 0 && (
-				<>
-					<h2 className="mb-2 h5">💪🏻 Stuff I got to do</h2>
-					<TodoList
-						todos={incompleteTodos}
-						onDelete={handleDeleteTodo}
-						onToggle={handleToggleTodo}
-					/>
-
-					<h2 className="mb-2 h5">🥺 Stuff I've done</h2>
-					<TodoList
-						todos={completedTodos}
-						onDelete={handleDeleteTodo}
-						onToggle={handleToggleTodo}
-					/>
-
-					<TodoCounter
-						completed={completedTodos.length}
-						total={todos.length}
-					/>
-				</>
-			)}
-
-			{!error && !isLoading && todos.length === 0 && (
-				<Alert variant="warning">
-					You ain't got no todos 🤔?
-				</Alert>
-			)}
-		</Container>
+			<Container className="container py-2">
+				<Routes>
+					<Route path='/' element={<HomePage />} />
+					<Route path='/todos' element={<TodosPage />} />
+				</Routes>
+			</Container>
+		</div>
 	);
-}
+};
 
 export default App;
---
+++ 06-better-todos/src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+
+const HomePage = () => {
+  return (
+	<div>
+		<>
+			<h1>Welcome to better todos!</h1>
+
+			<p>Because when life gives you assignment, you need a todo list!</p>
+		</>
+	</div>
+  )
+}
+
+export default HomePage
