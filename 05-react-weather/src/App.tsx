af8a8f952006199c9abe7f9068f80fd78af1a0a9
lisamaliqi <lisamaliqi@hotmail.com>
2025-05-09 09:45:11 +0200

    catch errors when fetching weather data

--- 05-react-weather/src/App.tsx
+++ 05-react-weather/src/App.tsx
@@ -10,19 +10,37 @@ function App() {
 
 	const [ currentWeather, setCurrentWeather ] = useState<WeatherReport | null>(null);
 	const [ isLoading, setIsLoading ] = useState(false);
+	const [ error, setError ] = useState<string | false>(false);
 
 	const handleSearch = async (location: string) => {
 		// console.log('Someone wants the weather for...: ', location);
 		//show the loading airplane and remove previous location data if there is any
+		//set error to false when doing a new request
 		setIsLoading(true);
 		setCurrentWeather(null);
+		setError(false);
 
-		//call API and ask for weather in 'location'
-		const data = await getCurrentWeather(location);
+		//handle error in try catch
+		try {
+			//call API and ask for weather in 'location'
+			const data = await getCurrentWeather(location);
 
-		//update current weather state with the weather in 'location'
-		setCurrentWeather(data);
-		setIsLoading(false); //remove it when data is found
+			//update current weather state with the weather in 'location'
+			setCurrentWeather(data);
+
+		} catch (err){
+			if(err instanceof Error) {
+				setError(err.message);
+			} else {
+				setError('something unexpected happened.');
+			};
+
+			//you could also do this instead of if statement
+			//setError(err instanceof Error ? err.message : 'Somehting unexpected happened.');
+		};
+
+		//remove it when data is found
+		setIsLoading(false);
 	};
 
 
@@ -30,6 +48,10 @@ function App() {
 	return (
 		<div id="app" className="container">
 			<SearchCity onSearch={handleSearch}/>
+
+			{error && (
+				<div className="alert alert-warning">{error}</div>
+			)}
 
 			{isLoading && <img src={imgAirplane} className="img-fluid py-5 w-75" />}
 
