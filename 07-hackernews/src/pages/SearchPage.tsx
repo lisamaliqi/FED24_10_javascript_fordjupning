a2aa3772c0f6788427c1b42fc1b8f38d95c788e2
lisamaliqi <lisamaliqi@hotmail.com>
2025-05-15 10:44:38 +0200

    Hacker News: Added states and handling of form submit.

--- 07-hackernews/src/pages/SearchPage.tsx
+++ 07-hackernews/src/pages/SearchPage.tsx
@@ -6,26 +6,61 @@ import Form from "react-bootstrap/Form";
 import ListGroup from "react-bootstrap/ListGroup";
 
 const SearchPage = () => {
+	const [error, setError] = useState<string | false>(false);
+	const [isLoading, setIsLoading] = useState(false);
+	const [inputSearch, setInputSearch] = useState("");
+	const [searchResult, setSearchResult] = useState(null);  // fix me
+
+
+	const handleSubmit = async (e: React.FormEvent) => {
+		e.preventDefault();
+
+		// prevent smol searches
+		if (inputSearch.trim().length < 2) {
+			alert("Too short search query!");
+			return;
+		};
+
+		// search for haxx0rs 🕵🏻‍♂️
+		console.log('WOuld search for ' + inputSearch + ' in HN API');
+
+	};
+
+
+
+
 	return (
 		<>
 			<h1>🔎🔦👀</h1>
 
-			<Form className="mb-4">
+			<Form className="mb-4" onSubmit={handleSubmit}>
 				<Form.Group className="mb-3" controlId="searchQuery">
 					<Form.Label>Search Query</Form.Label>
-					<Form.Control type="text" placeholder="Enter your search query" />
+					<Form.Control
+						onChange={e => setInputSearch(e.target.value)}
+						value={inputSearch}
+						type="text"
+						placeholder="Enter your search query"
+						required
+					/>
 				</Form.Group>
 
 				<div className="d-flex justify-content-end">
-					<Button variant="success" type="submit">
+					<Button
+						disabled={inputSearch.trim().length < 2}
+						variant="success"
+						type="submit"
+					>
 						Search
 					</Button>
 				</div>
 			</Form>
 
-			{false && <Alert variant='warning'>DIS MY ERROR</Alert>}
+			{/* {false && <Alert variant='warning'>DIS MY ERROR</Alert>} */}
+			{error && <Alert variant="warning">{error}</Alert>}
 
-			{false && <p>🤔 Loading...</p>}
+			{/* {false && <p>🤔 Loading...</p>} */}
+			{isLoading && <p>🤔 Loading...</p>}
 
 			{true && (
 				<div id="search-result">
@@ -40,6 +75,7 @@ const SearchPage = () => {
 						))}
 					</ListGroup>
 
+					{/* Change page to not load in all search result in the first page (save storage) */}
 					<div className="d-flex justify-content-between align-items-center">
 						<div className="prev">
 							<Button variant="primary">Previous Page</Button>
