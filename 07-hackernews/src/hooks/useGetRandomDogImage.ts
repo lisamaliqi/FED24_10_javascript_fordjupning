ffe4eab35958bd1b67a7bd7af71d04e845483b72
lisamaliqi <lisamaliqi@hotmail.com>
2025-05-20 14:27:43 +0200

    Pass url to fetch to our custom hook so it reacts to changes of said URL.

--- 07-hackernews/src/hooks/useGetRandomDogImage.ts
+++ 07-hackernews/src/hooks/useGetRandomDogImage.ts
@@ -3,27 +3,40 @@ import { RandomDogImage } from '../types/DogAPI.types';
 import axios from 'axios';
 
 
-const useGetRandomDogImage = () => {
+const useGetRandomDogImage = (url: string | null = null) => {
 	const [ data, setData ] = useState<RandomDogImage | null>(null); //null bc we haven't fetched a dog yet
+	const [ isLoading, setIsLoading ] = useState(false);
+
 
 	//fetch the data
-	const getData = async () => {
+	const getData = async (resource: string) => {
+		//reset state
+		setData(null);
+		setIsLoading(true);
+
+
 		//get the data from API
-		const res = await axios.get<RandomDogImage>('https://dog.ceo/api/breeds/image/random');
+		const res = await axios.get<RandomDogImage>(resource);
 		await new Promise(r => setTimeout(r, 1500))
 
 		//update state with data
 		setData(res.data);
+		setIsLoading(false);
 	};
 
 
 	useEffect(() => {
-		getData();
-	}, []);
+		if (!url) {
+			return;
+		};
+
+		getData(url);
+	}, [url]);
 
 
 	return {
 		data: data,
+		isLoading: isLoading,
 	};
 };
 
--- 07-hackernews/src/pages/RandomDogPage.tsx
+++ 07-hackernews/src/pages/RandomDogPage.tsx
@@ -1,18 +1,29 @@
+import Button from "react-bootstrap/Button";
 import Image from "react-bootstrap/Image";
 import useGetRandomDogImage from "../hooks/useGetRandomDogImage";
+import { useState } from "react";
 
 
 
 const RandomDogPage = () => {
-
+	const [ url, setUrl] = useState<string | null>('https://dog.ceo/api/breeds/image/random');
 	//destructing
-	const { data } = useGetRandomDogImage();
+	const { data, isLoading } = useGetRandomDogImage(url);
 
 	return (
 		<>
 			<h1>A random little doggi🐶</h1>
 
-			{!data && <p>fetching doggo...</p>}
+			<div className="mb-3">
+				<Button
+					onClick={() => setUrl('https://dog.ceo/api/breeds/image/random')}
+				>Random Doggo</Button>
+				<Button
+					onClick={() => setUrl('https://dog.ceo/api/breed/mix/images/random')}
+				>Random Mix doggo</Button>
+			</div>
+
+			{isLoading && <p>fetching doggo...</p>}
 
 			{data && data.status === 'success' && (
 				<div>
