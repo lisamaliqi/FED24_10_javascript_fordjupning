f44376db232cef1ca704cf13fa74b5d981c272f1
lisamaliqi <lisamaliqi@hotmail.com>
2025-06-02 11:17:45 +0200

    Configured QueryClient to not refetch on window focus and also consider cached data stale after 15 seconds (instead of the default which is that data is immediately considered stale after it has been fetched).

--- 08-react-query/src/main.tsx
+++ 08-react-query/src/main.tsx
@@ -4,7 +4,14 @@ import { BrowserRouter } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.tsx";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false, //don't refetch when user refocuses on app (maybe if they leave the tab and then come back)
+			staleTime: 1000 * 15, // 15 seconds, won't refresh data automatically within 15 sec bc it is seen as fresh
+		},
+	},
+});
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
