cfc91534ad188f382c50aedc559fae1c4fb152ae
lisamaliqi <lisamaliqi@hotmail.com>
2025-06-02 11:12:58 +0200

    Added the same query on HomePage to demonstrate that React Query provides the cached data and then refetches new data in the background.

--- 08-react-query/src/pages/HomePage.tsx
+++ 08-react-query/src/pages/HomePage.tsx
@@ -1,8 +1,22 @@
+import { getRandomDadJoke } from "../services/ICanHazDadJokeAPI";
+import { useQuery } from "@tanstack/react-query";
+
 const HomePage = () => {
+
+	//this demonstrates that the joke inside dadjoke page is the same as this joke, it doesn't refetch all the time
+	const { data } = useQuery({
+		queryKey: ["dadjoke"],
+		queryFn: getRandomDadJoke,
+	});
+
 	return (
 		<>
 			<h1>I ❤️ React Query</h1>
 			<p>Welcome to my homepage. Please write in the guestbook.</p>
+
+			<div>
+				<p className="display-5 text-center my-5">{data && data.joke}</p>
+			</div>
 		</>
 	);
 };
