899b4208464e919f194b950da2c3389d396861db
lisamaliqi <lisamaliqi@hotmail.com>
2025-06-04 11:43:08 +0200

    Get todos from the cache if they exist and are fresh, otherwise fetch them. And ONLY add the new todo to the todos-array if it doesn't already exist there.

--- 09-react-query-todos/src/pages/CreateTodoPage.tsx
+++ 09-react-query-todos/src/pages/CreateTodoPage.tsx
@@ -15,18 +15,28 @@ export default function CreateTodoPage() {
 
 	const createTodoMutation = useMutation({
 		mutationFn: TodosAPI.postTodo,
-		onSuccess: (createdTodo) => {
+		onSuccess: async (createdTodo) => {
 			// set the response from the mutation as the query cache for the created todo
 			queryClient.setQueryData(["todo", { id: createdTodo.id }], createdTodo);
 
+			//get ['todos'] from the cache if they exist and are fresh,
+			//otherwise fetch them from the API
+			const cachedTodos = await queryClient.fetchQuery({
+				queryKey: ["todos"],
+				queryFn: TodosAPI.getTodos,
+			});
+
 			// instead of invalidating the ["todos"] query, we can construct new data
 			// based on the previous data + newly created todo from the mutation
-			queryClient.setQueryData<Todo[]>(["todos"], (todos) => {
-				return [
-					...todos ?? [],
-					createdTodo,
-				];
-			});
+			//**ONLY** add the new todo if it does not already exist in the cache
+			if (!cachedTodos.find(todo => todo.id === createdTodo.id)) {
+				queryClient.setQueryData<Todo[]>(["todos"], (todos) => {
+					return [
+						...todos ?? [],
+						createdTodo,
+					];
+				});
+			}
 		},
 	});
 
