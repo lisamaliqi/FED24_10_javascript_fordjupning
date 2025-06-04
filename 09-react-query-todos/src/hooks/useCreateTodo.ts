38c913844067649d8edf95168342cb6f0ebaddcd
lisamaliqi <lisamaliqi@hotmail.com>
2025-06-04 15:06:44 +0200

    Added useCreateTodo custom hook.

---
+++ 09-react-query-todos/src/hooks/useCreateTodo.ts
@@ -0,0 +1,40 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import * as TodosAPI from "../services/TodosAPI";
+import { Todo } from "../services/Todo.types";
+
+
+const useCreateTodo = () => {
+
+	const queryClient  = useQueryClient();
+
+
+	return useMutation({
+		mutationFn: TodosAPI.postTodo,
+		onSuccess: async (createdTodo) => {
+			// set the response from the mutation as the query cache for the created todo
+			queryClient.setQueryData(["todo", { id: createdTodo.id }], createdTodo);
+
+			//get ['todos'] from the cache if they exist and are fresh,
+			//otherwise fetch them from the API
+			const cachedTodos = await queryClient.fetchQuery({
+				queryKey: ["todos"],
+				queryFn: TodosAPI.getTodos,
+			});
+
+			// instead of invalidating the ["todos"] query, we can construct new data
+			// based on the previous data + newly created todo from the mutation
+			//**ONLY** add the new todo if it does not already exist in the cache
+			if (!cachedTodos.find(todo => todo.id === createdTodo.id)) {
+				queryClient.setQueryData<Todo[]>(["todos"], (todos) => {
+					return [
+						...todos ?? [],
+						createdTodo,
+					];
+				});
+			}
+		},
+	})
+};
+
+
+export default useCreateTodo;
--- 09-react-query-todos/src/pages/CreateTodoPage.tsx
+++ 09-react-query-todos/src/pages/CreateTodoPage.tsx
@@ -1,52 +1,20 @@
 import { Link } from "react-router";
 import Alert from "react-bootstrap/Alert";
 import AddTodoForm from "../components/AddTodoForm";
-import { NewTodo, Todo } from "../services/Todo.types";
-import * as TodosAPI from '../services/TodosAPI';
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { NewTodo } from "../services/Todo.types";
 import ErrorAlert from "../components/Alerts/ErrorAlerts";
+import useCreateTodo from "../hooks/useCreateTodo";
 
 
 export default function CreateTodoPage() {
 
-	// const navigate = useNavigate();
-	const queryClient  = useQueryClient();
-
-
-	const createTodoMutation = useMutation({
-		mutationFn: TodosAPI.postTodo,
-		onSuccess: async (createdTodo) => {
-			// set the response from the mutation as the query cache for the created todo
-			queryClient.setQueryData(["todo", { id: createdTodo.id }], createdTodo);
-
-			//get ['todos'] from the cache if they exist and are fresh,
-			//otherwise fetch them from the API
-			const cachedTodos = await queryClient.fetchQuery({
-				queryKey: ["todos"],
-				queryFn: TodosAPI.getTodos,
-			});
-
-			// instead of invalidating the ["todos"] query, we can construct new data
-			// based on the previous data + newly created todo from the mutation
-			//**ONLY** add the new todo if it does not already exist in the cache
-			if (!cachedTodos.find(todo => todo.id === createdTodo.id)) {
-				queryClient.setQueryData<Todo[]>(["todos"], (todos) => {
-					return [
-						...todos ?? [],
-						createdTodo,
-					];
-				});
-			}
-		},
-	});
-
+	const createTodoMutation = useCreateTodo();
 
 	const handleCreateTodo = async (title: string) => {
 		const todo: NewTodo = {
 			title,
 			completed: false,
 		}
-
 
 		// Call mutation 🐢☢️
 		createTodoMutation.mutate(todo);
