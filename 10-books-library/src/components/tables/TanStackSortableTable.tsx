f21252ce1a76c78d6677557e30d4104a3b85a600
lisamaliqi <lisamaliqi@hotmail.com>
2025-06-12 12:06:48 +0200

    Save sort-direction to a variable so we don't have to call getIsSorted() twice and confuse TypeScript.

--- 10-books-library/src/components/tables/TanStackSortableTable.tsx
+++ 10-books-library/src/components/tables/TanStackSortableTable.tsx
@@ -1,5 +1,5 @@
 import BSTable from "react-bootstrap/Table";
-import { ColumnDef, flexRender, getCoreRowModel, getSortedRowModel, SortDirection, SortingState, useReactTable } from "@tanstack/react-table";
+import { ColumnDef, flexRender, getCoreRowModel, getSortedRowModel, SortingState, useReactTable } from "@tanstack/react-table";
 import { useState } from "react";
 
 
@@ -39,27 +39,31 @@ const TanStackBasicTable = <TData, TValue>({ columns, data }: TanStackBasicTable
 	return (
 		<BSTable bordered hover responsive striped size="sm">
 			<thead>
-				{table.getHeaderGroups().map(headerGroup => ( //get the headerGroup from the table and map over it (includes accessorKey and header)
+				{table.getHeaderGroups().map(headerGroup => (
 					<tr key={headerGroup.id}>
-						{headerGroup.headers.map(header => ( //map over the headerGroups headers
-							<th key={header.id} colSpan={header.colSpan}>
-								{header.isPlaceholder
-									? null
-									: <div /* Sort ID, name and birthday */
-										className={header.column.getCanSort() ? "sortable" : ""}
-										onClick={header.column.getToggleSortingHandler()}
-									>
-										{flexRender(
-											header.column.columnDef.header,
-											header.getContext()
-										)}
+						{headerGroup.headers.map(header => {
+							const sortDirection = header.column.getIsSorted();
 
-										{/* Add a sort direction indicator */}
-										{header.column.getIsSorted() && sortingIndicators[header.column.getIsSorted() as SortDirection]}
-									</div>
-								}
-							</th>
-						))}
+							return (
+								<th key={header.id} colSpan={header.colSpan}>
+									{header.isPlaceholder
+										? null
+										: <div
+											className={header.column.getCanSort() ? "sortable" : ""}
+											onClick={header.column.getToggleSortingHandler()}
+										>
+											{flexRender(
+												header.column.columnDef.header,
+												header.getContext()
+											)}
+
+											{/* Add a sort direction indicator */}
+											{sortDirection && sortingIndicators[sortDirection]}
+										</div>
+									}
+								</th>
+							)
+						})}
 					</tr>
 				))}
 			</thead>
